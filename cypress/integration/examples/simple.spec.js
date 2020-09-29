d9a17fa8d7a878ef47163b429ec62a1e5336c498
Jelena Marjanovic <jelena.marjanovic.13@gmail.com>
2020-09-29 19:21:04 +0200

    Challenge - Check if displayed price of the specific book is correct

--- cypress/integration/examples/simple.spec.js
+++ cypress/integration/examples/simple.spec.js
@@ -16,3 +16,35 @@ describe('Browser Actions', () => {
     cy.get('.product_pod').its('length').should('eq', 11);
   });
 });
+
+describe('Challenge Time! Create more Test Scenarios', () => {
+  it('should load books website', () => {
+    cy.visit('http://books.toscrape.com/index.html', { timeout: 5000 });
+
+    cy.url().should('include', 'index.html');
+  });
+
+  it('should click on Poetry category', () => {
+    cy.get('a').contains('Poetry').click();
+
+    // Verify we landed on the right page
+    cy.get('h1').contains('Poetry');
+  });
+
+  it('should click on "Olio" book image', () => {
+    cy.get('[alt="Olio"]').click();
+  });
+
+  it('should go back to Poetry page', () => {
+    cy.go('back');
+  });
+
+  it('should click on "Olio" book link', () => {
+    cy.get('a').contains('Olio').click();
+  });
+
+  it('should check if the displayed price is correct', () => {
+    // cy.get('.price_color').first().should('have.text', '£23.88');
+    cy.get('.price_color').contains('£23.88');
+  });
+});
