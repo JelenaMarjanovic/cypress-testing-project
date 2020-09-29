42ee4cbd7e96ea045b1d12a1c1c1a8a13e4247b1
Jelena Marjanovic <jelena.marjanovic.13@gmail.com>
2020-09-29 14:18:58 +0200

    Check correct URL

--- cypress/integration/examples/simple.spec.js
+++ cypress/integration/examples/simple.spec.js
@@ -2,4 +2,8 @@ describe('Browser Actions', () => {
   it('should load correct url', () => {
     cy.visit('http://example.com/', { timeout: 10000 });
   });
+
+  it('should check correct url', () => {
+    cy.url().should('include', 'example.com');
+  });
 });
