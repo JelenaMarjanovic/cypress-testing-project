512aea5b020a113dfda2e16e78b8efaa6bffc301
Jelena Marjanovic <jelena.marjanovic.13@gmail.com>
2020-09-30 01:01:53 +0200

    Clear & type text into inputs, submit form and check for error messages

---
+++ cypress/integration/examples/login.spec.js
@@ -0,0 +1,31 @@
+describe('Working with inputs', () => {
+  it('should load login page', () => {
+    cy.visit('http://zero.webappsecurity.com/login.html');
+
+    // Verify that we landed on the correct page
+    cy.url().should('include', 'login.html');
+  });
+
+  it('should fill username', () => {
+    cy.get('#user_login').clear();
+
+    cy.get('#user_login').type('Some Invalid Name', { delay: 50 });
+  });
+
+  it('should fill password', () => {
+    cy.get('#user_password').clear();
+
+    cy.get('#user_password').type('Some Invalid Password', { delay: 50 });
+  });
+
+  it('should submit login form', () => {
+    cy.contains('Sign in').click();
+  });
+
+  it('should display error message', () => {
+    cy.get('.alert-error').should('be.visible');
+
+    // Should display correct error message
+    cy.get('.alert-error').contains('Login and/or password are wrong.');
+  });
+});
