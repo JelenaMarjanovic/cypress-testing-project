bf5602b7084a3b7fc50730c826e6b8ee5900b375
Jelena Marjanovic <jelena.marjanovic.13@gmail.com>
2020-09-30 20:13:56 +0200

    Viewport & devices emulation

---
+++ cypress/integration/examples/device.spec.js
@@ -0,0 +1,41 @@
+describe('Device Tests', () => {
+  it('720p', () => {
+    cy.viewport(1280, 720);
+
+    cy.visit('http://www.example.com');
+
+    cy.wait(3000);
+  });
+
+  it('1080p - full HD', () => {
+    cy.viewport(1980, 1080);
+
+    cy.visit('http://www.example.com');
+
+    cy.wait(3000);
+  });
+
+  it('iPhone X', () => {
+    cy.viewport('iphone-x');
+
+    cy.visit('http://www.example.com');
+
+    cy.wait(3000);
+  });
+
+  it('iPad Mini', () => {
+    cy.viewport('ipad-mini');
+
+    cy.visit('http://www.example.com');
+
+    cy.wait(3000);
+  });
+
+  it('MacBook 15', () => {
+    cy.viewport('macbook-15');
+
+    cy.visit('http://www.example.com');
+
+    cy.wait(3000);
+  });
+});
