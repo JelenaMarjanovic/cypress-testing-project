4ebe7751457b0b28e974a94f8cf0d7f724af8933
Jelena Marjanovic <jelena.marjanovic.13@gmail.com>
2020-10-05 02:36:23 +0200

    Write data into TXT file

--- cypress/integration/examples/data.spec.js
+++ cypress/integration/examples/data.spec.js
@@ -2,4 +2,8 @@ describe('Write / Read Data to / from JSON / Text File', () => {
   it('should write data into JSON', () => {
     cy.writeFile('log.json', { name: 'Mike', age: 25 });
   });
+
+  it('should write data to the text file', () => {
+    cy.writeFile('log.txt', 'Hello world!');
+  });
 });
