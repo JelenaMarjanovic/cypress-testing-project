f4f2305110a4cc513b85d76e7b9250a25c78681a
Jelena Marjanovic <jelena.marjanovic.13@gmail.com>
2020-11-01 00:00:04 +0100

    Implement Cypress Dark Theme (Halloween theme as well)

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -14,7 +14,12 @@
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
-import './commands'
+import './commands';
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+
+require('cypress-dark');
+
+// Halloween theme
+// require('cypress-dark/src/halloween);
